e09c640c77d28c0a40935fe92eb7d44263052e82
vera-mvv <vera.malinovskaya@firstlinesoftware.com>
2022-05-08 20:23:36 +0300

    add layout and header

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Root } from './pages/root';
+import { Header } from './components/Header';
+import './App.scss';
 
 export const App = () => (
   <BrowserRouter>
-    <Root />
+    <Header />
+    <div className="layout">
+      <Root />
+    </div>
   </BrowserRouter>
 );
--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -4,9 +4,11 @@ import {
   Routes,
 } from 'react-router-dom';
 import { UiKit } from '../ui-kit';
+import { Main } from '../main';
 
 export const Root: FC = () => (
   <Routes>
+    <Route path="/" element={<Main />} />
     <Route path="/ui-kit" element={<UiKit />} />
   </Routes>
 );
